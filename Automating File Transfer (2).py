d941336ff4fac52de97772963838cdf1183b2949
Qasim278 <78903769+Qasim278@users.noreply.github.com>
2023-02-15 12:01:07 +0200

    Update Automating File Transfer (2).py

--- Automating File Transfer (2).py
+++ Automating File Transfer (2).py
@@ -1,20 +1,4 @@
-''' You work at a company that receives daily data files from external partners. These files need to be processed and analyzed, but first, they need to be transferred to the company's internal network.
 
-The goal of this project is to automate the process of transferring the files from an external FTP server to the company's internal network.
-
-Here are the steps you can take to automate this process:
-
-    Use the ftplib library to connect to the external FTP server and list the files in the directory.
-
-    Use the os library to check for the existence of a local directory where the files will be stored.
-
-    Use a for loop to iterate through the files on the FTP server and download them to the local directory using the ftplib.retrbinary() method.
-
-    Use the shutil library to move the files from the local directory to the internal network.
-
-    Use the schedule library to schedule the script to run daily at a specific time.
-
-    You can also set up a log file to keep track of the files that have been transferred and any errors that may have occurred during the transfer process. '''
 
 # import libraries
 from ftplib import FTP
