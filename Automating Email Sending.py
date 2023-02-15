010a50361300eca1880b09811af57d5ceface3ad
Qasim278 <78903769+Qasim278@users.noreply.github.com>
2023-02-15 12:00:51 +0200

    Update and rename Automating Email Sending (1).py to Automating Email Sending.py

--- Automating Email Sending.py
+++ Automating Email Sending.py
@@ -1,18 +1,4 @@
-''' You work at a company that sends daily reports to clients via email. The goal of this project is to automate the process of sending these reports via email.
 
-Here are the steps you can take to automate this process:
-
-    Use the smtplib library to connect to the email server and send the emails.
-
-    Use the email library to compose the email, including the recipient's email address, the subject, and the body of the email.
-
-    Use the os library to access the report files that need to be sent.
-
-    Use a for loop to iterate through the list of recipients and send the email and attachment.
-
-    Use the schedule library to schedule the script to run daily at a specific time.
-
-    You can also set up a log file to keep track of the emails that have been sent and any errors that may have occurred during the email sending process. '''
 
 # before testing start python email server
 # python -m smtpd -c DebuggingServer -n localhost:1025
